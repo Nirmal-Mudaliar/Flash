e1edf342551248c473d5e4282ee5fe436e66692e
Nirmal-Mudaliar <nirmalmudaliar22@gmail.com>
2022-08-21 17:50:01 +0530

    implemented SearchNewsFragment.kt screen

--- app/src/main/java/io/nirmal/flash/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/io/nirmal/flash/ui/fragments/SearchNewsFragment.kt
@@ -1,17 +1,72 @@
 package io.nirmal.flash.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.nirmal.flash.R
+import io.nirmal.flash.adapter.NewsAdapter
 import io.nirmal.flash.databinding.FragmentSearchNewsBinding
+import io.nirmal.flash.ui.MainActivity
+import io.nirmal.flash.ui.viewModel.NewsViewModel
+import io.nirmal.flash.utils.Constants
+import io.nirmal.flash.utils.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment : Fragment() {
 
     private var _binding: FragmentSearchNewsBinding? = null
     private val binding get() = _binding!!
+
+    lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    val TAG = "SearchNewsFragment"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+        setupRecyclerView()
+        var job: Job? = null
+        binding.tedtSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(Constants.SEARCH_NEWS_TIME_DELAY)
+                if (editable.toString().isNotEmpty()) {
+                    viewModel.getAllSearchedNews(editable.toString())
+                }
+            }
+        }
+        viewModel.searchedNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occured: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+
+
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +75,22 @@ class SearchNewsFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentSearchNewsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding.progressBar.visibility = View.VISIBLE
     }
 
 }
--- app/src/main/java/io/nirmal/flash/utils/Constants.kt
+++ app/src/main/java/io/nirmal/flash/utils/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "303842bb34f149588f2c721743ed569e"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
